20b32023da1b67700c9a4a4c10838d5e3b009e41
MMulla <zore.mulla@gmail.com>
2019-09-21 19:37:20 +0300

    create data file

---
+++ components/CarDetail/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    maker: "JEEP",
+    model: "Wrangler",
+    color: "Yellow",
+    gear: "MANUAL",
+    seats: 4,
+    year: 2019,
+    milage: 50000,
+    price: "900.000",
+    image: "http://muffinbase.com/media/wrangler.jpeg"
+  },
+  {
+    maker: "NISSAN",
+    model: "Rogue",
+    color: "Red",
+    gear: "AUTOMATIC",
+    seats: 5,
+    year: 2019,
+    milage: 70000,
+    price: "250.000",
+    image: "http://muffinbase.com/media/nissan_rogue.jpeg"
+  },
+  {
+    maker: "MCLAREN",
+    model: "Mercedes Benz SLR",
+    color: "Navy Blue",
+    gear: "MANUAL",
+    seats: 3,
+    year: 2006,
+    milage: 15000,
+    price: "100.000",
+    image: "http://muffinbase.com/media/download.jpeg"
+  }
+];
+
+export default data;
